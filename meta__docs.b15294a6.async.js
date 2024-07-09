9002cd92f245b6265be181efa3bf9b815ab2fdfa
Eric <3234655977@qq.com>
2024-07-09 17:50:47 +0800

    1、增加丝带组件模块 2、调整说明文档格式

---
+++ meta__docs.b15294a6.async.js
@@ -0,0 +1,91 @@
+"use strict";(self.webpackChunkintellgence_ui=self.webpackChunkintellgence_ui||[]).push([[904],{82847:function(m,n,e){e.r(n),e.d(n,{demos:function(){return I}});var u=e(17061),o=e.n(u),p=e(17156),v=e.n(p),t=e(67294),I={"docs-components-badge-demo-0":{component:t.memo(t.lazy(v()(o()().mark(function s(){var i,l,c;return o()().wrap(function(r){for(;;)switch(r.prev=r.next){case 0:return r.next=2,Promise.resolve().then(e.bind(e,28986));case 2:return i=r.sent,l=i.Badge,c={fontSize:"16px",backgroundColor:"#cccccc",width:"50px",height:"50px",display:"flex",alignItems:"center",justifyContent:"center"},r.abrupt("return",{default:function(){return t.createElement("div",null,t.createElement(l,{count:"3",color:"red"},t.createElement("div",{style:c},"Box"))," ","\xA0\xA0",t.createElement(l,{count:"10000",color:"#52c41a"},t.createElement("div",{div:!0,style:c},"Box"))," \xA0\xA0",t.createElement(l,{overflowCount:"10",count:"10000",color:"#faad14"},t.createElement("div",{div:!0,style:c},"Box"))," ","\xA0\xA0")}});case 6:case"end":return r.stop()}},s)})))),asset:{type:"BLOCK",id:"docs-components-badge-demo-0",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:`import { Badge } from 'intellgence_ui';
+
+const divStyle: React.CSSProperties = {
+  fontSize: '16px',
+  backgroundColor: '#cccccc',
+  width: '50px',
+  height: '50px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+export default () => (
+  <div>
+    <Badge count="3" color="red">
+      <div style={divStyle}>Box</div>
+    </Badge>{' '}
+    &nbsp;&nbsp;
+    <Badge count="10000" color="#52c41a">
+      <div div style={divStyle}>
+        Box
+      </div>
+    </Badge> &nbsp;&nbsp;
+    <Badge overflowCount="10" count="10000" color="#faad14">
+      <div div style={divStyle}>
+        Box
+      </div>
+    </Badge>{' '}
+    &nbsp;&nbsp;
+  </div>
+);`},intellgence_ui:{type:"NPM",value:"0.1.5"}},entry:"index.tsx"},context:{intellgence_ui:e(28986)},renderOpts:{compile:function(){var s=v()(o()().mark(function l(){var c,d=arguments;return o()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return a.next=2,e.e(19).then(e.bind(e,4019));case 2:return a.abrupt("return",(c=a.sent).default.apply(c,d));case 3:case"end":return a.stop()}},l)}));function i(){return s.apply(this,arguments)}return i}()}}}},64527:function(m,n,e){e.r(n),e.d(n,{demos:function(){return I}});var u=e(17061),o=e.n(u),p=e(17156),v=e.n(p),t=e(67294),I={"docs-components-button-demo-0":{component:t.memo(t.lazy(v()(o()().mark(function s(){var i,l;return o()().wrap(function(d){for(;;)switch(d.prev=d.next){case 0:return d.next=2,Promise.resolve().then(e.bind(e,28986));case 2:return i=d.sent,l=i.Button,d.abrupt("return",{default:function(){return t.createElement("div",null,t.createElement(l,{type:"primary",text:"Primary"})," \xA0 \xA0",t.createElement(l,{type:"info",text:"Info"})," \xA0 \xA0",t.createElement(l,{type:"success",text:"Success"})," \xA0 \xA0",t.createElement(l,{type:"warning",text:"Warning"})," \xA0 \xA0",t.createElement(l,{type:"danger",text:"Danger"}))}});case 5:case"end":return d.stop()}},s)})))),asset:{type:"BLOCK",id:"docs-components-button-demo-0",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:`import { Button } from 'intellgence_ui';
+
+export default () => (
+  <div>
+    <Button type="primary" text="Primary"></Button> &nbsp; &nbsp;
+    <Button type="info" text="Info"></Button> &nbsp; &nbsp;
+    <Button type="success" text="Success"></Button> &nbsp; &nbsp;
+    <Button type="warning" text="Warning"></Button> &nbsp; &nbsp;
+    <Button type="danger" text="Danger"></Button>
+  </div>
+);`},intellgence_ui:{type:"NPM",value:"0.1.5"}},entry:"index.tsx"},context:{intellgence_ui:e(28986)},renderOpts:{compile:function(){var s=v()(o()().mark(function l(){var c,d=arguments;return o()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return a.next=2,e.e(19).then(e.bind(e,4019));case 2:return a.abrupt("return",(c=a.sent).default.apply(c,d));case 3:case"end":return a.stop()}},l)}));function i(){return s.apply(this,arguments)}return i}()}}}},18819:function(m,n,e){e.r(n),e.d(n,{demos:function(){return o}});var u=e(67294),o={}},18762:function(m,n,e){e.r(n),e.d(n,{demos:function(){return o}});var u=e(67294),o={}},97696:function(m,n,e){e.r(n),e.d(n,{demos:function(){return I}});var u=e(17061),o=e.n(u),p=e(17156),v=e.n(p),t=e(67294),I={"docs-components-ribbon-demo-0":{component:t.memo(t.lazy(v()(o()().mark(function s(){var i,l,c;return o()().wrap(function(r){for(;;)switch(r.prev=r.next){case 0:return r.next=2,Promise.resolve().then(e.bind(e,28986));case 2:return i=r.sent,l=i.Ribbon,c={fontSize:"16px",height:"30px",lineHeight:"30px",border:"1px solid #ccc",padding:"5px",borderRadius:"4px",marginBottom:"10px"},r.abrupt("return",{default:function(){return t.createElement("div",null,t.createElement(l,{text:"Hippies"},t.createElement("div",{style:c},"Where there is a will there is a way")),t.createElement(l,{text:"Hippies",color:"red"},t.createElement("div",{style:c},"Where there is a will there is a way")),t.createElement(l,{text:"Hippies",color:"pink"},t.createElement("div",{style:c},"Where there is a will there is a way")))}});case 6:case"end":return r.stop()}},s)})))),asset:{type:"BLOCK",id:"docs-components-ribbon-demo-0",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:`import { Ribbon } from 'intellgence_ui';
+
+const divStyle: React.CSSProperties = {
+  fontSize: '16px',
+  height: '30px',
+  lineHeight: '30px',
+  border: '1px solid #ccc',
+  padding: '5px',
+  borderRadius: '4px',
+  marginBottom: '10px',
+};
+
+export default () => (
+  <div>
+    <Ribbon text="Hippies">
+      <div style={divStyle}>Where there is a will there is a way</div>
+    </Ribbon>
+    <Ribbon text="Hippies" color="red">
+      <div style={divStyle}>Where there is a will there is a way</div>
+    </Ribbon>
+    <Ribbon text="Hippies" color="pink">
+      <div style={divStyle}>Where there is a will there is a way</div>
+    </Ribbon>
+  </div>
+);`},intellgence_ui:{type:"NPM",value:"0.1.5"}},entry:"index.tsx"},context:{intellgence_ui:e(28986)},renderOpts:{compile:function(){var s=v()(o()().mark(function l(){var c,d=arguments;return o()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return a.next=2,e.e(19).then(e.bind(e,4019));case 2:return a.abrupt("return",(c=a.sent).default.apply(c,d));case 3:case"end":return a.stop()}},l)}));function i(){return s.apply(this,arguments)}return i}()}}}},72566:function(m,n,e){e.r(n),e.d(n,{demos:function(){return I}});var u=e(17061),o=e.n(u),p=e(17156),v=e.n(p),t=e(67294),I={"docs-components-tag-demo-0":{component:t.memo(t.lazy(v()(o()().mark(function s(){var i,l;return o()().wrap(function(d){for(;;)switch(d.prev=d.next){case 0:return d.next=2,Promise.resolve().then(e.bind(e,28986));case 2:return i=d.sent,l=i.Tag,d.abrupt("return",{default:function(){return t.createElement("div",null,t.createElement(l,{color:"default",text:"default"})," \xA0",t.createElement(l,{color:"magenta",text:"magenta"})," \xA0",t.createElement(l,{color:"red",text:"red"})," \xA0",t.createElement(l,{color:"volcano",text:"volcano"})," \xA0",t.createElement(l,{color:"orange",text:"orange"})," \xA0",t.createElement(l,{color:"gold",text:"gold"})," \xA0",t.createElement(l,{color:"lime",text:"lime"})," \xA0",t.createElement(l,{color:"green",text:"green"})," \xA0",t.createElement(l,{color:"cyan",text:"cyan"})," \xA0",t.createElement(l,{color:"blue",text:"blue"})," \xA0",t.createElement(l,{color:"geekblue",text:"geekblue"})," \xA0",t.createElement(l,{color:"purple",text:"purple"})," \xA0")}});case 5:case"end":return d.stop()}},s)})))),asset:{type:"BLOCK",id:"docs-components-tag-demo-0",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:`import { Tag } from 'intellgence_ui';
+
+export default () => (
+  <div>
+    <Tag color="default" text="default"></Tag> &nbsp;
+    <Tag color="magenta" text="magenta"></Tag> &nbsp;
+    <Tag color="red" text="red"></Tag> &nbsp;
+    <Tag color="volcano" text="volcano"></Tag> &nbsp;
+    <Tag color="orange" text="orange"></Tag> &nbsp;
+    <Tag color="gold" text="gold"></Tag> &nbsp;
+    <Tag color="lime" text="lime"></Tag> &nbsp;
+    <Tag color="green" text="green"></Tag> &nbsp;
+    <Tag color="cyan" text="cyan"></Tag> &nbsp;
+    <Tag color="blue" text="blue"></Tag> &nbsp;
+    <Tag color="geekblue" text="geekblue"></Tag> &nbsp;
+    <Tag color="purple" text="purple"></Tag> &nbsp;
+  </div>
+);`},intellgence_ui:{type:"NPM",value:"0.1.5"}},entry:"index.tsx"},context:{intellgence_ui:e(28986)},renderOpts:{compile:function(){var s=v()(o()().mark(function l(){var c,d=arguments;return o()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return a.next=2,e.e(19).then(e.bind(e,4019));case 2:return a.abrupt("return",(c=a.sent).default.apply(c,d));case 3:case"end":return a.stop()}},l)}));function i(){return s.apply(this,arguments)}return i}()}},"docs-components-tag-demo-1":{component:t.memo(t.lazy(v()(o()().mark(function s(){var i,l;return o()().wrap(function(d){for(;;)switch(d.prev=d.next){case 0:return d.next=2,Promise.resolve().then(e.bind(e,28986));case 2:return i=d.sent,l=i.Tag,d.abrupt("return",{default:function(){return t.createElement("div",null,t.createElement(l,{color:"#f50",text:"#f50"})," \xA0",t.createElement(l,{color:"#2db7f5",text:"#2db7f5"})," \xA0",t.createElement(l,{color:"#87d068",text:"#87d068"})," \xA0",t.createElement(l,{color:"#108ee9",text:"#108ee9"})," \xA0")}});case 5:case"end":return d.stop()}},s)})))),asset:{type:"BLOCK",id:"docs-components-tag-demo-1",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:`import { Tag } from 'intellgence_ui';
+
+export default () => (
+  <div>
+    <Tag color="#f50" text="#f50"></Tag> &nbsp;
+    <Tag color="#2db7f5" text="#2db7f5"></Tag> &nbsp;
+    <Tag color="#87d068" text="#87d068"></Tag> &nbsp;
+    <Tag color="#108ee9" text="#108ee9"></Tag> &nbsp;
+  </div>
+);`},intellgence_ui:{type:"NPM",value:"0.1.5"}},entry:"index.tsx"},context:{intellgence_ui:e(28986)},renderOpts:{compile:function(){var s=v()(o()().mark(function l(){var c,d=arguments;return o()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return a.next=2,e.e(19).then(e.bind(e,4019));case 2:return a.abrupt("return",(c=a.sent).default.apply(c,d));case 3:case"end":return a.stop()}},l)}));function i(){return s.apply(this,arguments)}return i}()}}}},6244:function(m,n,e){e.r(n),e.d(n,{demos:function(){return o}});var u=e(67294),o={}},69812:function(m,n,e){e.r(n),e.d(n,{demos:function(){return o}});var u=e(67294),o={}},29766:function(m,n,e){e.r(n),e.d(n,{demos:function(){return o}});var u=e(67294),o={}},81487:function(m,n,e){e.r(n),e.d(n,{demos:function(){return o}});var u=e(67294),o={}},28986:function(m,n,e){e.r(n),e.d(n,{Badge:function(){return v},Button:function(){return I},Ribbon:function(){return i},Tag:function(){return c}});var u=e(67294),o=e(85893),p=function(r){var a="intel-badge-sup",f=99,x={};return r.color&&(x.backgroundColor=r.color),r.overflowCount&&(f=r.overflowCount),(0,o.jsxs)("span",{className:"intel-badge",children:[r.children,(0,o.jsx)("sup",{className:a,style:x,children:r.count>f?"".concat(f,"+"):r.count})]})},v=p,t=function(r){var a="";switch(r.type){case"primary":a="intel-btn-blue intel-btn";break;case"info":a="intel-btn-gray intel-btn";break;case"success":a="intel-btn-green intel-btn";break;case"warning":a="intel-btn-warning intel-btn";break;case"danger":a="intel-btn-red intel-btn";break;default:a="intel-btn-gray intel-btn"}return(0,o.jsx)("button",{type:r.isSubmit===!0?"submit":"button",className:"".concat(a),onClick:r.onClick,children:r.text})},I=t,s=function(r){var a="intel-ribbon-end";r.placement==="start"&&(a="intel-ribbon-start"),a+=" item-ribbon-box";var f={};return r.color&&(f={color:r.color,backgroundColor:r.color}),(0,o.jsxs)("div",{className:"intel-ribbon",children:[r.children,(0,o.jsxs)("div",{style:f,className:a,children:[(0,o.jsx)("span",{className:"intel-ribbon-text",children:r.text}),(0,o.jsx)("div",{className:"intel-ribbon-corner"})]})]})},i=s,l=function(r){var a="intel-tag",f="",x=["magenta","red","volcano","orange","gold","lime","green","cyan","blue","geekblue","purple"],y=x.find(function(E){return E===r.color}),g;return y?(g={},f="intel-tag-".concat(r.color)):(g={backgroundColor:r.color,borderColor:r.color},r.color.includes("#")&&(g.color="white")),(0,o.jsx)("span",{onClick:r.onClick,className:[a,f].join(" "),style:g,children:r.text})},c=l},63874:function(m,n,e){e.r(n),e.d(n,{texts:function(){return u}});const u=[{value:"\u53C2\u6570",paraId:0,tocIndex:2},{value:"\u8BF4\u660E",paraId:0,tocIndex:2},{value:"\u7C7B\u578B",paraId:0,tocIndex:2},{value:"\u9ED8\u8BA4\u503C",paraId:0,tocIndex:2},{value:"count",paraId:0,tocIndex:2},{value:"\u5C55\u793A\u7684\u6570\u5B57\uFF0C\u5927\u4E8E overflowCount \u65F6\u663E\u793A\u4E3A ${overflowCount}+\uFF0C\u4E3A 0 \u65F6\u9690\u85CF",paraId:0,tocIndex:2},{value:"number",paraId:0,tocIndex:2},{value:"99",paraId:0,tocIndex:2},{value:"overflowCount",paraId:0,tocIndex:2},{value:"\u5C01\u9876\u7684\u6570\u5B57\u503C",paraId:0,tocIndex:2},{value:"number",paraId:0,tocIndex:2},{value:"-",paraId:0,tocIndex:2},{value:"color",paraId:0,tocIndex:2},{value:"\u5FBD\u6807\u989C\u8272",paraId:0,tocIndex:2},{value:"string",paraId:0,tocIndex:2},{value:"red",paraId:0,tocIndex:2}]},31527:function(m,n,e){e.r(n),e.d(n,{texts:function(){return u}});const u=[{value:"\u53C2\u6570",paraId:0,tocIndex:2},{value:"\u8BF4\u660E",paraId:0,tocIndex:2},{value:"\u7C7B\u578B",paraId:0,tocIndex:2},{value:"\u9ED8\u8BA4\u503C",paraId:0,tocIndex:2},{value:"text",paraId:0,tocIndex:2},{value:"\u6309\u94AE\u6587\u6848",paraId:0,tocIndex:2},{value:"string",paraId:0,tocIndex:2},{value:"-",paraId:0,tocIndex:2},{value:"type",paraId:0,tocIndex:2},{value:"\u6309\u94AE\u7C7B\u578B ",paraId:0,tocIndex:2},{value:"primary",paraId:0,tocIndex:2},{value:" ",paraId:0,tocIndex:2},{value:"info",paraId:0,tocIndex:2},{value:" ",paraId:0,tocIndex:2},{value:"warning",paraId:0,tocIndex:2},{value:" ",paraId:0,tocIndex:2},{value:"success",paraId:0,tocIndex:2},{value:" ",paraId:0,tocIndex:2},{value:"danger",paraId:0,tocIndex:2},{value:"string",paraId:0,tocIndex:2},{value:"info",paraId:0,tocIndex:2},{value:"isSubmit",paraId:0,tocIndex:2},{value:"\u662F\u5426\u8FDB\u884C\u63D0\u4EA4",paraId:0,tocIndex:2},{value:"boolean",paraId:0,tocIndex:2},{value:"false",paraId:0,tocIndex:2},{value:"onClick",paraId:0,tocIndex:2},{value:"\u5BF9\u5916\u66B4\u9732\u7684\u70B9\u51FB\u4E8B\u4EF6",paraId:0,tocIndex:2},{value:"Object",paraId:0,tocIndex:2},{value:"-",paraId:0,tocIndex:2}]},17959:function(m,n,e){e.r(n),e.d(n,{texts:function(){return u}});const u=[]},53190:function(m,n,e){e.r(n),e.d(n,{texts:function(){return u}});const u=[]},8249:function(m,n,e){e.r(n),e.d(n,{texts:function(){return u}});const u=[{value:"\u53C2\u6570",paraId:0,tocIndex:2},{value:"\u8BF4\u660E",paraId:0,tocIndex:2},{value:"\u7C7B\u578B",paraId:0,tocIndex:2},{value:"\u9ED8\u8BA4\u503C",paraId:0,tocIndex:2},{value:"text",paraId:0,tocIndex:2},{value:"\u4E1D\u5E26\u4E2D\u5C55\u793A\u5185\u5BB9",paraId:0,tocIndex:2},{value:"string",paraId:0,tocIndex:2},{value:"-",paraId:0,tocIndex:2},{value:"placement",paraId:0,tocIndex:2},{value:"\u4E1D\u5E26\u4F4D\u7F6E",paraId:0,tocIndex:2},{value:"start",paraId:0,tocIndex:2},{value:" ",paraId:0,tocIndex:2},{value:"end",paraId:0,tocIndex:2},{value:"end",paraId:0,tocIndex:2},{value:"color",paraId:0,tocIndex:2},{value:"\u4E1D\u5E26\u989C\u8272",paraId:0,tocIndex:2},{value:"string",paraId:0,tocIndex:2},{value:"#1677ff;",paraId:0,tocIndex:2}]},86657:function(m,n,e){e.r(n),e.d(n,{texts:function(){return u}});const u=[{value:"\u53C2\u6570",paraId:0,tocIndex:3},{value:"\u8BF4\u660E",paraId:0,tocIndex:3},{value:"\u7C7B\u578B",paraId:0,tocIndex:3},{value:"\u9ED8\u8BA4\u503C",paraId:0,tocIndex:3},{value:"text",paraId:0,tocIndex:3},{value:"Tag \u6587\u6848",paraId:0,tocIndex:3},{value:"string",paraId:0,tocIndex:3},{value:"-",paraId:0,tocIndex:3},{value:"color",paraId:0,tocIndex:3},{value:"Tag \u989C\u8272 \uFF08\u652F\u6301\u9ED8\u8BA4 color \u4E0E\u81EA\u5B9A\u4E49\u989C\u8272\uFF09",paraId:0,tocIndex:3},{value:"string",paraId:0,tocIndex:3},{value:"-",paraId:0,tocIndex:3},{value:"closeIcon",paraId:0,tocIndex:3},{value:"\u662F\u5426\u5C55\u793A\u5173\u95ED\u6309\u94AE",paraId:0,tocIndex:3},{value:"boolean",paraId:0,tocIndex:3},{value:"false",paraId:0,tocIndex:3},{value:"onClick",paraId:0,tocIndex:3},{value:"\u5BF9\u5916\u66B4\u9732\u7684\u70B9\u51FB\u4E8B\u4EF6",paraId:0,tocIndex:3},{value:"Object",paraId:0,tocIndex:3},{value:"-",paraId:0,tocIndex:3}]},95244:function(m,n,e){e.r(n),e.d(n,{texts:function(){return u}});const u=[]},74610:function(m,n,e){e.r(n),e.d(n,{texts:function(){return u}});const u=[{value:"Intel. \u5168\u79F0 Intellgeence UI \u57FA\u4E8E React TS \u6253\u9020\u7684\u65E0\u9700\u4F9D\u8D56\u7B2C\u4E09\u65B9 UI \u5E93\uFF0C\u652F\u6301\u6309\u9700\u5BFC\u5165\u3002",paraId:0,tocIndex:0},{value:"\u5982\u679C\u5728\u4F7F\u7528\u8FC7\u7A0B\u4E2D\u53D1\u73B0\u4EFB\u4F55\u95EE\u9898\u3001\u6216\u8005\u6709\u6539\u5584\u5EFA\u8BAE\uFF0C\u6B22\u8FCE\u5728 GitHub Issues \u8FDB\u884C\u53CD\u9988\uFF1A",paraId:1,tocIndex:2},{value:"https://github.com/EricGSX/Intellgeence-UI/issues",paraId:1,tocIndex:2}]},3234:function(m,n,e){e.r(n),e.d(n,{texts:function(){return u}});const u=[]},29110:function(m,n,e){e.r(n),e.d(n,{texts:function(){return u}});const u=[]}}]);
