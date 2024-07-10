74cacfdfb3475dda62cb6c0c30e806605831b6d1
Eric <3234655977@qq.com>
2024-07-10 17:53:45 +0800

    V0.2.2  增加水印组件

---
+++ meta__docs.99a5406f.async.js
@@ -0,0 +1,105 @@
+"use strict";(self.webpackChunkspotless_ui=self.webpackChunkspotless_ui||[]).push([[904],{82847:function(v,t,e){e.r(t),e.d(t,{demos:function(){return x}});var d=e(17061),r=e.n(d),f=e(17156),p=e.n(f),n=e(67294),x={"docs-components-badge-demo-0":{component:n.memo(n.lazy(p()(r()().mark(function c(){var u,a,s;return r()().wrap(function(m){for(;;)switch(m.prev=m.next){case 0:return m.next=2,Promise.resolve().then(e.bind(e,91440));case 2:return u=m.sent,a=u.Badge,s={fontSize:"16px",backgroundColor:"#cccccc",width:"50px",height:"50px",display:"flex",alignItems:"center",justifyContent:"center"},m.abrupt("return",{default:function(){return n.createElement("div",null,n.createElement(a,{count:"3",color:"red"},n.createElement("div",{style:s},"Box"))," ","\xA0\xA0",n.createElement(a,{count:"10000",color:"#52c41a"},n.createElement("div",{div:!0,style:s},"Box"))," \xA0\xA0",n.createElement(a,{overflowCount:"10",count:"10000",color:"#faad14"},n.createElement("div",{div:!0,style:s},"Box"))," ","\xA0\xA0")}});case 6:case"end":return m.stop()}},c)})))),asset:{type:"BLOCK",id:"docs-components-badge-demo-0",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:`import { Badge } from 'spotless-ui';
+
+const divStyle: React.CSSProperties = {
+  fontSize: '16px',
+  backgroundColor: '#cccccc',
+  width: '50px',
+  height: '50px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+export default () => (
+  <div>
+    <Badge count="3" color="red">
+      <div style={divStyle}>Box</div>
+    </Badge>{' '}
+    &nbsp;&nbsp;
+    <Badge count="10000" color="#52c41a">
+      <div div style={divStyle}>
+        Box
+      </div>
+    </Badge> &nbsp;&nbsp;
+    <Badge overflowCount="10" count="10000" color="#faad14">
+      <div div style={divStyle}>
+        Box
+      </div>
+    </Badge>{' '}
+    &nbsp;&nbsp;
+  </div>
+);`},"spotless-ui":{type:"NPM",value:"0.2.2"}},entry:"index.tsx"},context:{"spotless-ui":e(91440)},renderOpts:{compile:function(){var c=p()(r()().mark(function a(){var s,l=arguments;return r()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return o.next=2,e.e(19).then(e.bind(e,4019));case 2:return o.abrupt("return",(s=o.sent).default.apply(s,l));case 3:case"end":return o.stop()}},a)}));function u(){return c.apply(this,arguments)}return u}()}}}},64527:function(v,t,e){e.r(t),e.d(t,{demos:function(){return x}});var d=e(17061),r=e.n(d),f=e(17156),p=e.n(f),n=e(67294),x={"docs-components-button-demo-0":{component:n.memo(n.lazy(p()(r()().mark(function c(){var u,a;return r()().wrap(function(l){for(;;)switch(l.prev=l.next){case 0:return l.next=2,Promise.resolve().then(e.bind(e,91440));case 2:return u=l.sent,a=u.Button,l.abrupt("return",{default:function(){return n.createElement("div",null,n.createElement(a,{type:"primary",text:"Primary"})," \xA0 \xA0",n.createElement(a,{type:"info",text:"Info"})," \xA0 \xA0",n.createElement(a,{type:"success",text:"Success"})," \xA0 \xA0",n.createElement(a,{type:"warning",text:"Warning"})," \xA0 \xA0",n.createElement(a,{type:"danger",text:"Danger"}))}});case 5:case"end":return l.stop()}},c)})))),asset:{type:"BLOCK",id:"docs-components-button-demo-0",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:`import { Button } from 'spotless-ui';
+
+export default () => (
+  <div>
+    <Button type="primary" text="Primary"></Button> &nbsp; &nbsp;
+    <Button type="info" text="Info"></Button> &nbsp; &nbsp;
+    <Button type="success" text="Success"></Button> &nbsp; &nbsp;
+    <Button type="warning" text="Warning"></Button> &nbsp; &nbsp;
+    <Button type="danger" text="Danger"></Button>
+  </div>
+);`},"spotless-ui":{type:"NPM",value:"0.2.2"}},entry:"index.tsx"},context:{"spotless-ui":e(91440)},renderOpts:{compile:function(){var c=p()(r()().mark(function a(){var s,l=arguments;return r()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return o.next=2,e.e(19).then(e.bind(e,4019));case 2:return o.abrupt("return",(s=o.sent).default.apply(s,l));case 3:case"end":return o.stop()}},a)}));function u(){return c.apply(this,arguments)}return u}()}}}},18819:function(v,t,e){e.r(t),e.d(t,{demos:function(){return r}});var d=e(67294),r={}},18762:function(v,t,e){e.r(t),e.d(t,{demos:function(){return r}});var d=e(67294),r={}},97696:function(v,t,e){e.r(t),e.d(t,{demos:function(){return x}});var d=e(17061),r=e.n(d),f=e(17156),p=e.n(f),n=e(67294),x={"docs-components-ribbon-demo-0":{component:n.memo(n.lazy(p()(r()().mark(function c(){var u,a,s;return r()().wrap(function(m){for(;;)switch(m.prev=m.next){case 0:return m.next=2,Promise.resolve().then(e.bind(e,91440));case 2:return u=m.sent,a=u.Ribbon,s={fontSize:"16px",height:"30px",lineHeight:"30px",border:"1px solid #ccc",padding:"5px",borderRadius:"4px",marginBottom:"10px"},m.abrupt("return",{default:function(){return n.createElement("div",null,n.createElement(a,{text:"Hippies"},n.createElement("div",{style:s},"Where there is a will there is a way")),n.createElement(a,{text:"Hippies",color:"red"},n.createElement("div",{style:s},"Where there is a will there is a way")),n.createElement(a,{text:"Hippies",color:"pink"},n.createElement("div",{style:s},"Where there is a will there is a way")))}});case 6:case"end":return m.stop()}},c)})))),asset:{type:"BLOCK",id:"docs-components-ribbon-demo-0",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:`import { Ribbon } from 'spotless-ui';
+
+const divStyle: React.CSSProperties = {
+  fontSize: '16px',
+  height: '30px',
+  lineHeight: '30px',
+  border: '1px solid #ccc',
+  padding: '5px',
+  borderRadius: '4px',
+  marginBottom: '10px',
+};
+
+export default () => (
+  <div>
+    <Ribbon text="Hippies">
+      <div style={divStyle}>Where there is a will there is a way</div>
+    </Ribbon>
+    <Ribbon text="Hippies" color="red">
+      <div style={divStyle}>Where there is a will there is a way</div>
+    </Ribbon>
+    <Ribbon text="Hippies" color="pink">
+      <div style={divStyle}>Where there is a will there is a way</div>
+    </Ribbon>
+  </div>
+);`},"spotless-ui":{type:"NPM",value:"0.2.2"}},entry:"index.tsx"},context:{"spotless-ui":e(91440)},renderOpts:{compile:function(){var c=p()(r()().mark(function a(){var s,l=arguments;return r()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return o.next=2,e.e(19).then(e.bind(e,4019));case 2:return o.abrupt("return",(s=o.sent).default.apply(s,l));case 3:case"end":return o.stop()}},a)}));function u(){return c.apply(this,arguments)}return u}()}}}},72566:function(v,t,e){e.r(t),e.d(t,{demos:function(){return x}});var d=e(17061),r=e.n(d),f=e(17156),p=e.n(f),n=e(67294),x={"docs-components-tag-demo-0":{component:n.memo(n.lazy(p()(r()().mark(function c(){var u,a;return r()().wrap(function(l){for(;;)switch(l.prev=l.next){case 0:return l.next=2,Promise.resolve().then(e.bind(e,91440));case 2:return u=l.sent,a=u.Tag,l.abrupt("return",{default:function(){return n.createElement("div",null,n.createElement(a,{color:"default",text:"default"})," \xA0",n.createElement(a,{color:"magenta",text:"magenta"})," \xA0",n.createElement(a,{color:"red",text:"red"})," \xA0",n.createElement(a,{color:"volcano",text:"volcano"})," \xA0",n.createElement(a,{color:"orange",text:"orange"})," \xA0",n.createElement(a,{color:"gold",text:"gold"})," \xA0",n.createElement(a,{color:"lime",text:"lime"})," \xA0",n.createElement(a,{color:"green",text:"green"})," \xA0",n.createElement(a,{color:"cyan",text:"cyan"})," \xA0",n.createElement(a,{color:"blue",text:"blue"})," \xA0",n.createElement(a,{color:"geekblue",text:"geekblue"})," \xA0",n.createElement(a,{color:"purple",text:"purple"})," \xA0")}});case 5:case"end":return l.stop()}},c)})))),asset:{type:"BLOCK",id:"docs-components-tag-demo-0",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:`import { Tag } from 'spotless-ui';
+
+export default () => (
+  <div>
+    <Tag color="default" text="default"></Tag> &nbsp;
+    <Tag color="magenta" text="magenta"></Tag> &nbsp;
+    <Tag color="red" text="red"></Tag> &nbsp;
+    <Tag color="volcano" text="volcano"></Tag> &nbsp;
+    <Tag color="orange" text="orange"></Tag> &nbsp;
+    <Tag color="gold" text="gold"></Tag> &nbsp;
+    <Tag color="lime" text="lime"></Tag> &nbsp;
+    <Tag color="green" text="green"></Tag> &nbsp;
+    <Tag color="cyan" text="cyan"></Tag> &nbsp;
+    <Tag color="blue" text="blue"></Tag> &nbsp;
+    <Tag color="geekblue" text="geekblue"></Tag> &nbsp;
+    <Tag color="purple" text="purple"></Tag> &nbsp;
+  </div>
+);`},"spotless-ui":{type:"NPM",value:"0.2.2"}},entry:"index.tsx"},context:{"spotless-ui":e(91440)},renderOpts:{compile:function(){var c=p()(r()().mark(function a(){var s,l=arguments;return r()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return o.next=2,e.e(19).then(e.bind(e,4019));case 2:return o.abrupt("return",(s=o.sent).default.apply(s,l));case 3:case"end":return o.stop()}},a)}));function u(){return c.apply(this,arguments)}return u}()}},"docs-components-tag-demo-1":{component:n.memo(n.lazy(p()(r()().mark(function c(){var u,a;return r()().wrap(function(l){for(;;)switch(l.prev=l.next){case 0:return l.next=2,Promise.resolve().then(e.bind(e,91440));case 2:return u=l.sent,a=u.Tag,l.abrupt("return",{default:function(){return n.createElement("div",null,n.createElement(a,{color:"#f50",text:"#f50"})," \xA0",n.createElement(a,{color:"#2db7f5",text:"#2db7f5"})," \xA0",n.createElement(a,{color:"#87d068",text:"#87d068"})," \xA0",n.createElement(a,{color:"#108ee9",text:"#108ee9"})," \xA0")}});case 5:case"end":return l.stop()}},c)})))),asset:{type:"BLOCK",id:"docs-components-tag-demo-1",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:`import { Tag } from 'spotless-ui';
+
+export default () => (
+  <div>
+    <Tag color="#f50" text="#f50"></Tag> &nbsp;
+    <Tag color="#2db7f5" text="#2db7f5"></Tag> &nbsp;
+    <Tag color="#87d068" text="#87d068"></Tag> &nbsp;
+    <Tag color="#108ee9" text="#108ee9"></Tag> &nbsp;
+  </div>
+);`},"spotless-ui":{type:"NPM",value:"0.2.2"}},entry:"index.tsx"},context:{"spotless-ui":e(91440)},renderOpts:{compile:function(){var c=p()(r()().mark(function a(){var s,l=arguments;return r()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return o.next=2,e.e(19).then(e.bind(e,4019));case 2:return o.abrupt("return",(s=o.sent).default.apply(s,l));case 3:case"end":return o.stop()}},a)}));function u(){return c.apply(this,arguments)}return u}()}}}},6244:function(v,t,e){e.r(t),e.d(t,{demos:function(){return x}});var d=e(17061),r=e.n(d),f=e(17156),p=e.n(f),n=e(67294),x={"docs-components-watermark-demo-0":{component:n.memo(n.lazy(p()(r()().mark(function c(){var u,a;return r()().wrap(function(l){for(;;)switch(l.prev=l.next){case 0:return l.next=2,Promise.resolve().then(e.bind(e,91440));case 2:return u=l.sent,a=u.Watermark,l.abrupt("return",{default:function(){return n.createElement("div",null,n.createElement(a,{content:"Spotless UI"},n.createElement("div",{style:{height:"300px"}},"Content")))}});case 5:case"end":return l.stop()}},c)})))),asset:{type:"BLOCK",id:"docs-components-watermark-demo-0",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:`import { Watermark } from 'spotless-ui';
+
+export default () => (
+  <div>
+    <Watermark content="Spotless UI">
+      <div
+        style={{
+          height: '300px',
+        }}
+      >
+        Content
+      </div>
+    </Watermark>
+  </div>
+);`},"spotless-ui":{type:"NPM",value:"0.2.2"}},entry:"index.tsx"},context:{"spotless-ui":e(91440)},renderOpts:{compile:function(){var c=p()(r()().mark(function a(){var s,l=arguments;return r()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return o.next=2,e.e(19).then(e.bind(e,4019));case 2:return o.abrupt("return",(s=o.sent).default.apply(s,l));case 3:case"end":return o.stop()}},a)}));function u(){return c.apply(this,arguments)}return u}()}}}},69812:function(v,t,e){e.r(t),e.d(t,{demos:function(){return r}});var d=e(67294),r={}},29766:function(v,t,e){e.r(t),e.d(t,{demos:function(){return r}});var d=e(67294),r={}},81487:function(v,t,e){e.r(t),e.d(t,{demos:function(){return r}});var d=e(67294),r={}},91440:function(v,t,e){e.r(t),e.d(t,{Badge:function(){return p},Button:function(){return x},Ribbon:function(){return u},Tag:function(){return s},Watermark:function(){return m}});var d=e(67294),r=e(85893),f=function(i){var I="intel-badge-sup",g=99,y={};return i.color&&(y.backgroundColor=i.color),i.overflowCount&&(g=i.overflowCount),(0,r.jsxs)("span",{className:"intel-badge",children:[i.children,(0,r.jsx)("sup",{className:I,style:y,children:i.count>g?"".concat(g,"+"):i.count})]})},p=f,n=function(i){var I="";switch(i.type){case"primary":I="intel-btn-blue intel-btn";break;case"info":I="intel-btn-gray intel-btn";break;case"success":I="intel-btn-green intel-btn";break;case"warning":I="intel-btn-warning intel-btn";break;case"danger":I="intel-btn-red intel-btn";break;default:I="intel-btn-gray intel-btn"}return(0,r.jsx)("button",{type:i.isSubmit===!0?"submit":"button",className:"".concat(I),onClick:i.onClick,children:i.text})},x=n,c=function(i){var I="intel-ribbon-end";i.placement==="start"&&(I="intel-ribbon-start"),I+=" item-ribbon-box";var g={};return i.color&&(g={color:i.color,backgroundColor:i.color}),(0,r.jsxs)("div",{className:"intel-ribbon",children:[i.children,(0,r.jsxs)("div",{style:g,className:I,children:[(0,r.jsx)("span",{className:"intel-ribbon-text",children:i.text}),(0,r.jsx)("div",{className:"intel-ribbon-corner"})]})]})},u=c,a=function(i){var I="intel-tag",g="",y=["magenta","red","volcano","orange","gold","lime","green","cyan","blue","geekblue","purple"],T=y.find(function(P){return P===i.color}),E;return T?(E={},g="intel-tag-".concat(i.color)):(E={backgroundColor:i.color,borderColor:i.color},i.color.includes("#")&&(E.color="white")),(0,r.jsx)("span",{onClick:i.onClick,className:[I,g].join(" "),style:E,children:i.text})},s=a,l=function(i){var I="Spotless UI",g=function(P){var b=document.createElement("canvas"),h=b.getContext("2d"),B="#ebebeb";return i.color&&(B=i.color),h?(b.width=200,b.height=100,h.font="16px Arial",h.rotate(-30*Math.PI/180),h.fillStyle=B,h.fillText(P,0,90,100),b.toDataURL()):""},y=g(I),T={backgroundImage:"url('".concat(y,"')")};return(0,r.jsxs)("div",{className:"intel-watermark",children:[i.children,(0,r.jsx)("div",{className:"intel-watermark-content",style:T})]})},m=l},63874:function(v,t,e){e.r(t),e.d(t,{texts:function(){return d}});const d=[{value:"\u53C2\u6570",paraId:0,tocIndex:2},{value:"\u8BF4\u660E",paraId:0,tocIndex:2},{value:"\u7C7B\u578B",paraId:0,tocIndex:2},{value:"\u9ED8\u8BA4\u503C",paraId:0,tocIndex:2},{value:"count",paraId:0,tocIndex:2},{value:"\u5C55\u793A\u7684\u6570\u5B57\uFF0C\u5927\u4E8E overflowCount \u65F6\u663E\u793A\u4E3A ${overflowCount}+\uFF0C\u4E3A 0 \u65F6\u9690\u85CF",paraId:0,tocIndex:2},{value:"number",paraId:0,tocIndex:2},{value:"99",paraId:0,tocIndex:2},{value:"overflowCount",paraId:0,tocIndex:2},{value:"\u5C01\u9876\u7684\u6570\u5B57\u503C",paraId:0,tocIndex:2},{value:"number",paraId:0,tocIndex:2},{value:"-",paraId:0,tocIndex:2},{value:"color",paraId:0,tocIndex:2},{value:"\u5FBD\u6807\u989C\u8272",paraId:0,tocIndex:2},{value:"string",paraId:0,tocIndex:2},{value:"red",paraId:0,tocIndex:2}]},31527:function(v,t,e){e.r(t),e.d(t,{texts:function(){return d}});const d=[{value:"\u53C2\u6570",paraId:0,tocIndex:2},{value:"\u8BF4\u660E",paraId:0,tocIndex:2},{value:"\u7C7B\u578B",paraId:0,tocIndex:2},{value:"\u9ED8\u8BA4\u503C",paraId:0,tocIndex:2},{value:"text",paraId:0,tocIndex:2},{value:"\u6309\u94AE\u6587\u6848",paraId:0,tocIndex:2},{value:"string",paraId:0,tocIndex:2},{value:"-",paraId:0,tocIndex:2},{value:"type",paraId:0,tocIndex:2},{value:"\u6309\u94AE\u7C7B\u578B ",paraId:0,tocIndex:2},{value:"primary",paraId:0,tocIndex:2},{value:" ",paraId:0,tocIndex:2},{value:"info",paraId:0,tocIndex:2},{value:" ",paraId:0,tocIndex:2},{value:"warning",paraId:0,tocIndex:2},{value:" ",paraId:0,tocIndex:2},{value:"success",paraId:0,tocIndex:2},{value:" ",paraId:0,tocIndex:2},{value:"danger",paraId:0,tocIndex:2},{value:"string",paraId:0,tocIndex:2},{value:"info",paraId:0,tocIndex:2},{value:"isSubmit",paraId:0,tocIndex:2},{value:"\u662F\u5426\u8FDB\u884C\u63D0\u4EA4",paraId:0,tocIndex:2},{value:"boolean",paraId:0,tocIndex:2},{value:"false",paraId:0,tocIndex:2},{value:"onClick",paraId:0,tocIndex:2},{value:"\u5BF9\u5916\u66B4\u9732\u7684\u70B9\u51FB\u4E8B\u4EF6",paraId:0,tocIndex:2},{value:"Object",paraId:0,tocIndex:2},{value:"-",paraId:0,tocIndex:2}]},17959:function(v,t,e){e.r(t),e.d(t,{texts:function(){return d}});const d=[]},53190:function(v,t,e){e.r(t),e.d(t,{texts:function(){return d}});const d=[]},8249:function(v,t,e){e.r(t),e.d(t,{texts:function(){return d}});const d=[{value:"\u53C2\u6570",paraId:0,tocIndex:2},{value:"\u8BF4\u660E",paraId:0,tocIndex:2},{value:"\u7C7B\u578B",paraId:0,tocIndex:2},{value:"\u9ED8\u8BA4\u503C",paraId:0,tocIndex:2},{value:"text",paraId:0,tocIndex:2},{value:"\u4E1D\u5E26\u4E2D\u5C55\u793A\u5185\u5BB9",paraId:0,tocIndex:2},{value:"string",paraId:0,tocIndex:2},{value:"-",paraId:0,tocIndex:2},{value:"placement",paraId:0,tocIndex:2},{value:"\u4E1D\u5E26\u4F4D\u7F6E",paraId:0,tocIndex:2},{value:"start",paraId:0,tocIndex:2},{value:" ",paraId:0,tocIndex:2},{value:"end",paraId:0,tocIndex:2},{value:"end",paraId:0,tocIndex:2},{value:"color",paraId:0,tocIndex:2},{value:"\u4E1D\u5E26\u989C\u8272",paraId:0,tocIndex:2},{value:"string",paraId:0,tocIndex:2},{value:"#1677ff;",paraId:0,tocIndex:2}]},86657:function(v,t,e){e.r(t),e.d(t,{texts:function(){return d}});const d=[{value:"\u53C2\u6570",paraId:0,tocIndex:3},{value:"\u8BF4\u660E",paraId:0,tocIndex:3},{value:"\u7C7B\u578B",paraId:0,tocIndex:3},{value:"\u9ED8\u8BA4\u503C",paraId:0,tocIndex:3},{value:"text",paraId:0,tocIndex:3},{value:"Tag \u6587\u6848",paraId:0,tocIndex:3},{value:"string",paraId:0,tocIndex:3},{value:"-",paraId:0,tocIndex:3},{value:"color",paraId:0,tocIndex:3},{value:"Tag \u989C\u8272 \uFF08\u652F\u6301\u9ED8\u8BA4 color \u4E0E\u81EA\u5B9A\u4E49\u989C\u8272\uFF09",paraId:0,tocIndex:3},{value:"string",paraId:0,tocIndex:3},{value:"-",paraId:0,tocIndex:3},{value:"closeIcon",paraId:0,tocIndex:3},{value:"\u662F\u5426\u5C55\u793A\u5173\u95ED\u6309\u94AE",paraId:0,tocIndex:3},{value:"boolean",paraId:0,tocIndex:3},{value:"false",paraId:0,tocIndex:3},{value:"onClick",paraId:0,tocIndex:3},{value:"\u5BF9\u5916\u66B4\u9732\u7684\u70B9\u51FB\u4E8B\u4EF6",paraId:0,tocIndex:3},{value:"Object",paraId:0,tocIndex:3},{value:"-",paraId:0,tocIndex:3}]},95244:function(v,t,e){e.r(t),e.d(t,{texts:function(){return d}});const d=[{value:"\u53C2\u6570",paraId:0,tocIndex:2},{value:"\u8BF4\u660E",paraId:0,tocIndex:2},{value:"\u7C7B\u578B",paraId:0,tocIndex:2},{value:"\u9ED8\u8BA4\u503C",paraId:0,tocIndex:2},{value:"content",paraId:0,tocIndex:2},{value:"\u6C34\u5370\u5185\u5BB9",paraId:0,tocIndex:2},{value:"number",paraId:0,tocIndex:2},{value:"99",paraId:0,tocIndex:2},{value:"color",paraId:0,tocIndex:2},{value:"\u6C34\u5370\u989C\u8272",paraId:0,tocIndex:2},{value:"string",paraId:0,tocIndex:2},{value:"#ebebeb",paraId:0,tocIndex:2}]},74610:function(v,t,e){e.r(t),e.d(t,{texts:function(){return d}});const d=[{value:"Spotless. \u5168\u79F0 Spotless UI \u57FA\u4E8E React\u3001 TS \u3001Less \u6253\u9020\u7684\u65E0\u9700\u4F9D\u8D56\u7B2C\u4E09\u65B9 UI \u5E93\uFF0C\u652F\u6301\u6309\u9700\u5BFC\u5165\u3002",paraId:0,tocIndex:0},{value:"\u5982\u679C\u5728\u4F7F\u7528\u8FC7\u7A0B\u4E2D\u53D1\u73B0\u4EFB\u4F55\u95EE\u9898\u3001\u6216\u8005\u6709\u6539\u5584\u5EFA\u8BAE\uFF0C\u6B22\u8FCE\u5728 GitHub Issues \u8FDB\u884C\u53CD\u9988\uFF1A",paraId:1,tocIndex:2},{value:"\u8054\u7CFB\u6211\u4EEC",paraId:1,tocIndex:2}]},3234:function(v,t,e){e.r(t),e.d(t,{texts:function(){return d}});const d=[]},29110:function(v,t,e){e.r(t),e.d(t,{texts:function(){return d}});const d=[]}}]);
